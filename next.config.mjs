07a6b2939aafd775c7959131b3813c108afc953c
fezamba <fepetitt@gmail.com>
2025-12-13 16:38:01 -0300

    VAAAAAI

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["@react-pdf/renderer"],
+  
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 export default nextConfig;
